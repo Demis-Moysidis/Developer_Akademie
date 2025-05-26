d0ad691e0f4a2ab91cc5ee6fd6d7ade7774c9ddf
Demis Moysidis <demis.moysidis@outlook.de>
2025-05-26 15:05:55 +0200

    working with list und for-loop

--- script.js
+++ script.js
@@ -126,26 +126,167 @@
 //     console.log(document.getElementById('input_id').value)
 // }
 
-let count = 0;
 
-function init(){
+
+// let count = 0;
+
+// function init(){
     
-    debuggerOutput('inti')
-    testTwo('testTwo')
+//     debuggerOutput('inti')
+//     testTwo('testTwo')
     
-}
+// }
 
-function testOne(){
-    debuggerOutput('testOne')
-}
+// function testOne(){
+//     debuggerOutput('testOne')
+// }
 
-function testTwo(text){
-    debuggerOutput(text)
+// function testTwo(text){
+//     debuggerOutput(text)
     
-}
+// }
 
-function debuggerOutput(text){
-    count++;
-    let debuggerLogRef = document.getElementById('debugger_log');
-    debuggerLogRef.innerHTML = count + text;
-}
+// function debuggerOutput(text){
+//     count++;
+//     let debuggerLogRef = document.getElementById('debugger_log');
+//     debuggerLogRef.innerHTML = count + text;
+// }
+
+
+// function containsElement(array, element){
+//     return array.includes(element)
+// }
+
+// console.log(containsElement(['Anna', 'Ben', 'Clara'], 'Ben')); // true
+
+// console.log(containsElement(['Anna', 'Ben', 'Clara'], 'Daniel')); // false
+
+
+
+
+
+// function findElementIndex(array, element){
+//     return array.indexOf(element)
+// }
+
+
+// console.log(findElementIndex([10, 20, 30, 40], 30)); // 2
+
+// console.log(findElementIndex([10, 20, 30, 40], 50)); // -1
+
+
+
+// function removeFirstElement(array){
+//     array.shift()
+//     return array
+// }
+
+// console.log(removeFirstElement([10, 20, 30, 40])); // [20, 30, 40]
+
+// console.log(removeFirstElement(['a', 'b', 'c', 'd'])); // ['b', 'c', 'd']
+
+
+
+// function addElementToStart(array, element){
+//     array.unshift(element)
+//     return array
+
+// }
+
+// console.log(addElementToStart([2, 3, 4], 1)); // [1, 2, 3, 4]
+
+// console.log(addElementToStart(['b', 'c', 'd'], 'a')); // ['a', 'b', 'c', 'd']
+
+
+
+// function getSubArray(array, start, end){
+//     return array.slice(start, end)
+// }
+
+
+// console.log(getSubArray([1, 2, 3, 4, 5], 1, 4)); // [2, 3, 4]
+
+// console.log(getSubArray(['a', 'b', 'c', 'd', 'e'], 0, 3)); // ['a', 'b', 'c']
+
+
+// function joinArray(array, separator){
+//     return array.join(separator)
+// }
+
+// console.log(joinArray(['apple', 'banana', 'cherry'], ', ')); // "apple, banana, cherry"
+
+// console.log(joinArray([1, 2, 3, 4], ' - ')); // "1 - 2 - 3 - 4"
+
+
+// function sumArray(array){
+//     let sum = 0;
+//     for (let index = 0; index < array.length; index++) {
+//         sum += array[index];
+//     }
+//     return sum
+// }
+
+// console.log(sumArray([3, 7, 1, 4])); // Erwartete Ausgabe: 15 (3 + 7 + 1 + 4)
+
+// console.log(sumArray([1, 2, 3, 4, 5])); // Erwartete Ausgabe: 15 (1 + 2 + 3 + 4 + 5)
+
+
+// function printNumbers(number){
+//     let num_list = []
+//     for (let index = 0; index < number; index++) {
+//         num_list[index] = index + 1
+//     }
+//     console.log(num_list.join(", "))
+// }
+
+// printNumbers(5); // Erwartete Ausgabe: 1, 2, 3, 4, 5
+
+// printNumbers(3); // Erwartete Ausgabe: 1, 2, 3
+
+
+
+// function printNumbersReverse(number){
+//     let num_list = []
+//     for (let index = 0; index < number; index++) {
+//         num_list[index] = number - index;
+//     }
+//     console.log(num_list.join(", "))
+// }
+
+// printNumbersReverse(5); // Erwartete Ausgabe: 5, 4, 3, 2, 1
+
+// printNumbersReverse(3); // Erwartete Ausgabe: 3, 2, 1
+
+
+
+// function printEveryThirdElement(array){
+
+//     let result_list = []
+//     for (let index = 0; index < array.length; index += 3) {
+//         result_list.push(array[index]);
+//     }
+//     console.log(result_list.join(", "))
+// }
+
+// printEveryThirdElement([1, 2, 3, 4, 5, 6, 7, 8, 9]); // Erwartete Ausgabe: 1, 4, 7
+
+// printEveryThirdElement(['a', 'b', 'c', 'd', 'e', 'f']); // Erwartete Ausgabe: 'a', 'd'
+
+
+
+// function isPrime(number){
+//     if(number <= 1){
+//         return false
+//     }
+
+//     for (let index = 2; index < number; index++) {
+//         if(number % index == 0){
+//             return false
+//         }
+//     }
+//     return true
+// }
+
+// console.log(isPrime(7)); // Erwartete Ausgabe: true (7 ist eine Primzahl)
+
+// console.log(isPrime(4)); // Erwartete Ausgabe: false (4 ist keine Primzahl)
