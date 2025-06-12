8c04ce31da7a9fda71f621ddb613b7f12061dd5d
Demis Moysidis <demis.moysidis@outlook.de>
2025-06-12 11:10:17 +0200

    exercise: Verschachtelte Objekte

--- script.js
+++ script.js
@@ -301,3 +301,106 @@ function initArrays(){
         
     }
 }
+
+
+// let objectTest = {
+//     'name': "Theo",
+//     'nameZeigen': function (){
+//         return this.name;
+//     }
+// }
+
+// console.log(objectTest['nameZeigen']())
+
+// let myObjectArr = [
+//     {
+//         "name":"Max",
+//         "is_a_good_guy": true 
+//     },
+//     {
+//         "name":"Peter",
+//         "is_a_good_guy": false 
+//     },
+//     {
+//         "name":"Arnold",
+//         "is_a_good_guy": true 
+//     },
+//     {
+//         "name":"Justus",
+//         "is_a_good_guy": true 
+//     },
+//     {
+//         "name":"Bombur",
+//         "is_a_good_guy": false 
+//     }
+// ];
+
+// console.log(myObjectArr.findIndex(obj => obj.name === "Justus"));
+// console.log(myObjectArr.filter(obj => obj.name === "Justus"))
+
+let library = {
+      "info": {
+        "name": "Stadtbibliothek",
+        "location": {
+          "city": "Musterstadt",
+          "coordinates": { "lat": 48.1351, "lon": 11.582 }
+        }
+      },
+      "sections": {
+        "fiction": [
+          {
+            "shelf": 1,
+            "book": {
+              "title": "Die Verwandlung",
+              "author": {
+                "name": "Franz Kafka",
+                "born": "1883-07-03",
+                "died": "1924-06-03"
+              },
+              "year": 1915,
+              "status": "ausgeliehen"
+            }
+          },
+          {
+            "shelf": 2,
+            "book": {
+              "title": "1984",
+              "author": {
+                "name": "George Orwell",
+                "born": "1903-06-25",
+                "died": "1950-01-21"
+              },
+              "year": 1949,
+              "status": "verfügbar"
+            }
+          }
+        ],
+        "nonFiction": [
+          {
+            "shelf": 3,
+            "book": {
+              "title": "Eine kurze Geschichte der Zeit",
+              "author": {
+                "name": "Stephen Hawking",
+                "born": "1942-01-08",
+                "died": "2018-03-14"
+              },
+              "year": 1988,
+              "status": "verfügbar"
+            }
+          }
+        ]
+    }
+}
+
+console.log(library.info.location.city);
+console.log(library.sections.fiction[1].book.status)
+
+let fictionBooks = [];
+for (let index = 0; index < library.sections.fiction.length; index++) {
+    const element = library.sections.fiction[index].book.title;
+    fictionBooks.push(element)
+    
+}
+
+console.log(fictionBooks);
