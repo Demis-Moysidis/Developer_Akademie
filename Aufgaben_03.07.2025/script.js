9aef5c3bb96ff401b5e765e57d92dcbd588342ef
Demis Moysidis <demis.moysidis@outlook.de>
2025-07-03 11:01:50 +0200

    exercise: Aufgaben_03.07.2025

---
+++ Aufgaben_03.07.2025/script.js
@@ -0,0 +1,11 @@
+let testArray = [1, 2, 4, 6, 7, 9];
+
+for (let i = 0; i < testArray.length; i++) {
+    console.log(testArray[i]);
+}
+
+if(testArray.length > 5){
+    console.log('größer als 5');
+}else{
+    console.log('kleiner oder gleich 5');
+}
